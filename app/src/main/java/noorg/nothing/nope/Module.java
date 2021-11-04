ad7d15fd8e269d2547bf06d163dc0bdbb67da37a
hostar <janstarka@gmail.com>
2021-11-04 19:54:44 +0100

    missing BufferedReader

--- app/src/main/java/noorg/nothing/nope/Module.java
+++ app/src/main/java/noorg/nothing/nope/Module.java
@@ -17,6 +17,7 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.PrintWriter;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
