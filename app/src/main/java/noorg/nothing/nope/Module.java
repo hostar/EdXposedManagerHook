d38429068d4aa6b354f833d694b5261f3ffc151b
hostar <janstarka@gmail.com>
2021-11-04 19:50:57 +0100

    missing InputStreamReader

--- app/src/main/java/noorg/nothing/nope/Module.java
+++ app/src/main/java/noorg/nothing/nope/Module.java
@@ -16,6 +16,7 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.PrintWriter;
+import java.io.InputStreamReader;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
